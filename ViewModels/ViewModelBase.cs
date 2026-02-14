6d974a113803d667382b7e5a1d66f7719a935e1e
Jacob <jacobmars05@use.startmail.com>
2026-02-15 08:15:55 +1000

    update

--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -4,4 +4,5 @@ namespace VirtualSoundboard.ViewModels;
 
 public class ViewModelBase : ObservableObject
 {
+    
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
@@ -14,6 +15,7 @@ using Avalonia.Threading;
 using CSharpAlgorithms;
 using CSharpAlgorithms.Audio;
 using CSharpAlgorithms.Collection;
+using CSharpAlgorithms.GC;
 using CSharpAlgorithms.Interfaces;
 using PortAudioSharp;
 
@@ -24,187 +26,291 @@ public partial class MainWindow : Window
     private static BleepPlayer bleepPlayer = new BleepPlayer();
 
     private static Dictionary<string, AudioClip> audioClipCache = [];
-    //private static Dictionary<string, AudioDevice> deviceDic = [];
     private static Dictionary<string, AudioPlayer> audioPlayerDic = [];
+
+    static DirectoryInfo audioClipDir = new DirectoryInfo("Audio Clips");
 
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = this;
 
-        //new GCNotifier();
+        new GCNotifyer()
+        {
+            keepAlive = true,
+        };
 
-        //AudioDevice audioDevice = AudioDevice.CreateNewDevice();
-        //AudioClip clip = AudioClip.FromMP3File("Audio Clips/Vine Boom Sound Effect.mp3").GetAwaiter().GetResult();
-        //AudioPlayer player = new AudioPlayer(clip);
-        //audioDevice.AddPlayer(player);
-        //player.Play();
-
-        DirectoryInfo audioClipDir = new DirectoryInfo("Audio Clips");
         LoadDirectory(audioClipDir);
+        UpdateNewDeviceDropdown();
 
         Closed += OnExit;
 
-        void LoadDirectory(DirectoryInfo dir)
+        //AddNewDevice("default");
+
+        AddDeviceButton.Click += (sender, e) => AddNewDevice(NewDeviceDropdown.SelectedItem as string);
+    }
+
+    void LoadDirectory(DirectoryInfo dir)
+    {
+        SoundBoard.Children.Clear();
+
+        if (dir != audioClipDir)
         {
-            SoundBoard.Children.Clear();
-
-            if (dir != audioClipDir)
+            Button previousDirButton = new Button
             {
-                Button previousDirButton = new Button
-                {
-                    Content = "<-",
-                    ContextMenu = new ContextMenu(),
-                    Margin = new Avalonia.Thickness(5),
-                    Padding = new Avalonia.Thickness(10, 5),
-                };
-                previousDirButton.Click += (sender, e) => LoadDirectory(dir.Parent);
+                Content = "<-",
+                ContextMenu = new ContextMenu(),
+                Margin = new Avalonia.Thickness(5),
+                Padding = new Avalonia.Thickness(10, 5),
+            };
+            previousDirButton.Click += (sender, e) => LoadDirectory(dir.Parent);
 
-                SoundBoard.Children.Add(previousDirButton);
-            }
+            SoundBoard.Children.Add(previousDirButton);
+        }
 
-            DirectoryInfo[] subDirs = dir.GetDirectories();
-            FileInfo[] files = dir.GetFiles();
+        DirectoryInfo[] subDirs = dir.GetDirectories();
+        FileInfo[] files = dir.GetFiles();
 
-            foreach (DirectoryInfo subDir in subDirs)
+        foreach (DirectoryInfo subDir in subDirs)
+        {
+            Button dirButton = new Button
             {
-                Button dirButton = new Button
-                {
-                    Content = subDir.Name,
-                    ContextMenu = new ContextMenu(),
-                    Margin = new Avalonia.Thickness(5),
-                    Padding = new Avalonia.Thickness(10, 5),
-                };
-                dirButton.Click += (sender, e) => LoadDirectory(subDir);
+                Content = subDir.Name,
+                ContextMenu = new ContextMenu(),
+                Margin = new Avalonia.Thickness(5),
+                Padding = new Avalonia.Thickness(10, 5),
+            };
+            dirButton.Click += (sender, e) => LoadDirectory(subDir);
 
-                SoundBoard.Children.Add(dirButton);
-            }
+            SoundBoard.Children.Add(dirButton);
+        }
 
-            foreach (FileInfo file in files)
+        foreach (FileInfo file in files)
+        {
+            StackPanel panel = new StackPanel { Orientation = Orientation.Horizontal };
+            Label label = new Label { Content = file.Name, Margin = new Avalonia.Thickness(5), VerticalAlignment = VerticalAlignment.Center };
+            panel.Children.Add(label);
+
+            Button playPauseButton = new Button
             {
-                StackPanel panel = new StackPanel { Orientation = Orientation.Horizontal };
-                Label label = new Label { Content = file.Name, Margin = new Avalonia.Thickness(5), VerticalAlignment = VerticalAlignment.Center };
-                panel.Children.Add(label);
+                Content = "▶",
+                ContextMenu = new ContextMenu(),
+                Margin = new Avalonia.Thickness(5),
+                Padding = new Avalonia.Thickness(10, 5),
+            };
 
-                Button playPauseButton = new Button
+            panel.Children.Add(playPauseButton);
+
+            playPauseButton.Click += async (sender, e) =>
+            {
+                foreach (AudioDevice device in DictionaryUtils.GetValues(AudioDevice.ActiveDevices))
                 {
-                    Content = "▶",
-                    ContextMenu = new ContextMenu(),
-                    Margin = new Avalonia.Thickness(5),
-                    Padding = new Avalonia.Thickness(10, 5),
-                };
-                panel.Children.Add(playPauseButton);
+                    AudioClip clip = null;
 
-                playPauseButton.Click += async (sender, e) =>
-                {
-                    string soundId = file.FullName;
-
-                    if (!audioClipCache.ContainsKey(soundId))
-                        audioClipCache[soundId] = await AudioClip.FromMP3File(soundId);
-
-                    AudioClip clip = audioClipCache[soundId];
-
-                    CSharpAlgorithms.Debug.Print(AudioDevice.ActiveDevices);
-                    foreach (AudioDevice device in DictionaryUtils.GetValues(AudioDevice.ActiveDevices))
+                    string soundId = $"{file.Name}:{device.Info.defaultSampleRate}";
+                    if (audioClipCache.ContainsKey(soundId))
+                        clip = audioClipCache[soundId];
+                    else
                     {
-                        string playerId = $"{device.Info.name} - {clip.Name}";
-                        if (audioPlayerDic.TryGetValue(playerId, out AudioPlayer player))
+                        clip = await AudioClip.FromMP3File(file.FullName, (int)device.Info.defaultSampleRate);
+                        audioClipCache[soundId] = clip;
+                    }
+
+
+                    string playerId = $"{device.Info.name} - {clip.Name}";
+                    if (audioPlayerDic.TryGetValue(playerId, out AudioPlayer player))
+                    {
+                        if (player.IsPlaying)
                         {
-                            player.Play();
+                            player.Pause();
                         }
                         else
                         {
-                            AudioPlayer newPlayer = new AudioPlayer(clip);
-                            newPlayer.OnStateChanged += (string state) =>
-                            {
-                                Dispatcher.UIThread.Post(() =>
-                                {
-                                    if (state == "play")
-                                        playPauseButton.Content = Text.PAUSE_SYMBOL;
-                                    else if (state == "pause" || state == "end")
-                                        playPauseButton.Content = Text.PLAY_SYMBOL;
-                                    else
-                                        playPauseButton.Content = "!";
-                                });
-                            };
-                            audioPlayerDic[playerId] = newPlayer;
-
-                            device.AddPlayer(newPlayer);
-                            newPlayer.Play();
+                            Console.WriteLine($"playing {playerId}");
+                            player.Play();
                         }
                     }
-                };
+                    else
+                    {
+                        AudioPlayer newPlayer = new AudioPlayer(clip);
+                        newPlayer.OnStateChanged += (string state) =>
+                        {
+                            Dispatcher.UIThread.Post(() =>
+                            {
+                                if (state == "play")
+                                    playPauseButton.Content = Text.PAUSE_SYMBOL;
+                                else if (state == "pause" || state == "end")
+                                    playPauseButton.Content = Text.PLAY_SYMBOL;
+                                else
+                                    playPauseButton.Content = "!";
+                            });
+                        };
+                        audioPlayerDic[playerId] = newPlayer;
 
-                SoundBoard.Children.Add(panel);
-            }
+                        device.audioPlayers.Add(newPlayer);
+                        newPlayer.Play();
+                    }
+                }
+            };
+
+            SoundBoard.Children.Add(panel);
         }
-
-        AudioDevice a = AddNewDevice("default");
-        //AudioDevice b = AddNewDevice("USB Audio Device: - (hw:1,0)");
-
-        //b.inputDevices.Add(a);
     }
 
-    AudioDevice AddNewDevice(string name = "")
+    void UpdateNewDeviceDropdown()
     {
-        AudioDevice device = AudioDevice.GetDevice(name);
+        string[] unusedDeviceNames = AudioDevice.GetNamesOfUnusedDevices();
+        NewDeviceDropdown.ItemsSource = unusedDeviceNames;
+        if (unusedDeviceNames.Length > 0)
+            NewDeviceDropdown.SelectedIndex = 0;
+    }
 
+    void AddAllDevices()
+    {
+        string[] unusedDeviceNames = AudioDevice.GetNamesOfUnusedDevices();
+        foreach (string name in unusedDeviceNames)
+            AddNewDevice(name);
+    }
+    void AddNewDevice(string name = "")
+    {
+        if (string.IsNullOrEmpty(name))
+            return;
+
+        if (AudioDevice.ActiveDevices.ContainsKey(name))
+            return;
+
+        UpdateNewDeviceDropdown();
+
+        if(AudioDevice.GetDevice(name, out AudioDevice device));
+            AddNewDevice(device);
+    }
+    void AddNewDevice(AudioDevice device)
+    {
         ComboBox nameDropdown = new ComboBox()
         {
             SelectedValue = device.Info.name,
             ItemsSource = AudioUtils.GetDeviceNames(),
         };
 
+        nameDropdown.SelectionChanged += (sender, e) =>
+        {
+            if (nameDropdown.SelectedValue is string selectedName)
+            {
+                if(!device.SetDevice(selectedName));
+                {
+                    nameDropdown.SelectedValue = device.Info.name;
+                }
+            }
+        };
+
+        StackPanel optionsPanel = new StackPanel { Orientation = Orientation.Vertical };
+
+        if (device.InputChannelCount > 0)
+        {
+            StackPanel inputOptionsPanel = new StackPanel { Orientation = Orientation.Vertical };
+            optionsPanel.Children.Add(inputOptionsPanel);
+
+            CheckBox enableInput = new CheckBox 
+            { 
+                Content = "Enable Input", 
+                Margin = new Avalonia.Thickness(5), 
+                IsChecked = device.IsInputMuted == false
+            };
+
+            inputOptionsPanel.Children.Add(enableInput);
+
+            Slider inputVolumeSlider = new Slider 
+            { 
+                Minimum = 0, 
+                Maximum = 10, 
+                Value = device.InputVolume, 
+                Margin = new Avalonia.Thickness(5) 
+            };
+            inputOptionsPanel.Children.Add(inputVolumeSlider);
+
+            inputVolumeSlider.PropertyChanged += (sender, e) =>
+            {
+                device.SetInputVolume((float)inputVolumeSlider.Value);
+            };
+        }
+
+        if (device.OutputChannelCount > 0)
+        {
+            StackPanel outputOptionsPanel = new StackPanel { Orientation = Orientation.Vertical };
+            optionsPanel.Children.Add(outputOptionsPanel);
+
+            CheckBox enableOutput = new CheckBox 
+            { 
+                Content = "Enable Output", 
+                Margin = new Avalonia.Thickness(5), 
+                IsChecked = device.IsOutputMuted == false 
+            };
+            enableOutput.PropertyChanged += (sender, e) =>
+            {
+                device.IsOutputMuted = !enableOutput.IsChecked.GetValueOrDefault();
+            };
+            outputOptionsPanel.Children.Add(enableOutput);
+
+            if (device.InputChannelCount > 0)
+            {
+                CheckBox playbackInput = new CheckBox 
+                { 
+                    Content = "Playback Input", 
+                    Margin = new Avalonia.Thickness(5), 
+                    IsChecked = device.IsPlayingInput
+                };
+                playbackInput.PropertyChanged += (sender, e) =>
+                {
+                    device.IsPlayingInput = playbackInput.IsChecked.GetValueOrDefault();
+                };
+
+                outputOptionsPanel.Children.Add(playbackInput);
+            }
+        }
+
+        ListBox inputDeviceListBox = new ListBox { Margin = new Avalonia.Thickness(5) };
+
         DeviceParent.Children.Add(new StackPanel
         {
             Children =
             {
-                nameDropdown
+                nameDropdown,
+                optionsPanel,
+                inputDeviceListBox
             }
         });
-
-        return device;
-    }
-
-    void AddNewDevice()
-    {
-        
     }
 
     private void OnExit(object? sender, EventArgs e)
     {
-        //SaveState();
+        SaveState();
     }
 
     private void SaveState()
     {
-        throw new NotImplementedException();
+        AudioDeviceData[] deviceDatas = AudioDeviceData.Get();
+        AudioDeviceData.Save("devices.json", deviceDatas);
     }
 
-    private void LoadState()
+    private bool LoadState()
     {
-        throw new NotImplementedException();
-    }
-    private void OnDragEnter(object? sender, DragEventArgs e)
-    {
-        e.DragEffects = e.Data.Contains(DataFormats.FileNames)
-            ? DragDropEffects.Copy
-            : DragDropEffects.None;
-    }
+        AudioDeviceData[] deviceDatas = AudioDeviceData.Load();
+        if (deviceDatas.Length == 0)
+            return false;
 
-    private void OnDragLeave(object? sender, RoutedEventArgs e)
-    {
-        // optional cleanup
-    }
-
-
-    private void OnDrop(object? sender, DragEventArgs e)
-    {
-        Console.WriteLine("Drop fired!");
-        if (e.Data.Contains(DataFormats.FileNames))
+        foreach (AudioDeviceData deviceData in deviceDatas)
         {
-            foreach (var f in e.Data.GetFileNames() ?? Array.Empty<string>())
-                Console.WriteLine($"Dropped: {f}");
+            if(!AudioDevice.GetDevice(deviceData.Name, out AudioDevice device))
+                continue;
+
+            device.SetInputVolume(deviceData.InputVolume);
+            device.SetOutputVolume(deviceData.OutputVolume);
+            device.IsMuted = deviceData.IsMuted;
+
+            AddNewDevice(device);
         }
+
+        return true;
     }
 }
 
